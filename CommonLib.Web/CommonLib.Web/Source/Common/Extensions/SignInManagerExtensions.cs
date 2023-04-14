260e3f5fd8b5ab37f73df02579960deebffd20b0
rvnlord <rvnlord@gmail.com>
2023-04-14 23:40:06 +0200

    `SignInOrTwoFactorAsync()` extension for identity Framework to skip redundant presign validation

---
+++ CommonLib.Web/CommonLib.Web/Source/Common/Extensions/SignInManagerExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+public static class SignInManagerExtensions
+{
+    public static async Task<SignInResult> SignInOrTwoFactorAsync<TUser>(this SignInManager<TUser> signInManager, TUser user, bool isPersistent, string loginProvider = null, bool bypassTwoFactor = false) where TUser : class
+    {
+        var miSignInOrTwoFactorAsync = typeof(SignInManager<TUser>).GetMethod("SignInOrTwoFactorAsync", BindingFlags.Instance | BindingFlags.NonPublic);
+        if (miSignInOrTwoFactorAsync is null)
+            throw new NullReferenceException("The method 'SignInOrTwoFactorAsync' was not found.");
+        return await ((Task<SignInResult>)miSignInOrTwoFactorAsync.Invoke(signInManager, new object[] { user, isPersistent, loginProvider, bypassTwoFactor }) ?? throw new NullReferenceException("The method 'SignInOrTwoFactorAsync' Task was null."));
+    }
+}
