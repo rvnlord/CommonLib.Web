91259045634a052b5da5647e05a9cbcb813ef9a6
rvnlord <rvnlord@gmail.com>
2023-05-13 21:23:30 +0200

    Add `AuthenticationSchemeConverter`

---
+++ CommonLib.Web/CommonLib.Web/Source/Common/Converters/AuthenticationSchemeConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CommonLib.Source.ViewModels.Account;
+using Microsoft.AspNetCore.Authentication;
+
+namespace CommonLib.Web.Source.Common.Converters
+{
+    public static class AuthenticationSchemeConverter
+    {
+        public static AuthenticationSchemeVM ToAuthenticationSchemeVM(this AuthenticationScheme authenticationScheme)
+        {
+            return new AuthenticationSchemeVM(authenticationScheme.Name, authenticationScheme.DisplayName, authenticationScheme.HandlerType);
+        }
+
+        public static List<AuthenticationSchemeVM> ToAuthenticationSchemeVMs(this IEnumerable<AuthenticationScheme> authenticationSchemes)
+        {
+            return authenticationSchemes.Select(a => a.ToAuthenticationSchemeVM()).ToList();
+        }
+    }
+}
