e06c0b4086180e1303adeb10db0268e99135c9a2
rvnlord <rvnlord@gmail.com>
2023-05-29 23:26:19 +0200

    `RouteDataExtensions` `GetRequiredString()` for both `Routing.RouteData ` and `Components.RouteData`

--- CommonLib.Web/CommonLib.Web/Source/Common/Extensions/RouteDataExtensions.cs
+++ CommonLib.Web/CommonLib.Web/Source/Common/Extensions/RouteDataExtensions.cs
@@ -1,16 +1,28 @@
 ﻿using System;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Routing;
 
 namespace CommonLib.Web.Source.Common.Extensions
 {
     public static class RouteDataExtensions
     {
-        public static string GetRequiredString(this RouteData routeData, string keyName)
+        public static string GetRequiredString(this Microsoft.AspNetCore.Routing.RouteData routeData, string keyName)
         {
             if (routeData == null)
                 throw new ArgumentNullException(nameof(routeData));
 
             if (!routeData.Values.TryGetValue(keyName, out var value))
+                throw new InvalidOperationException($"Could not find key with name '{keyName}'");
+
+            return value?.ToString();
+        }
+
+        public static string GetRequiredString(this Microsoft.AspNetCore.Components.RouteData routeData, string keyName)
+        {
+            if (routeData == null)
+                throw new ArgumentNullException(nameof(routeData));
+
+            if (!routeData.RouteValues.TryGetValue(keyName, out var value))
                 throw new InvalidOperationException($"Could not find key with name '{keyName}'");
 
             return value?.ToString();
