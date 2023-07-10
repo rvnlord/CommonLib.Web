b6db6266bfa2168f310b02d19835169d3f0e81f0
rvnlord <rvnlord@gmail.com>
2023-07-10 21:53:43 +0200

    Add `ExtStepper` Component

---
+++ CommonLib.Web/CommonLib.Web/Source/Common/Components/ExtStepperComponent/ExtStepperBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using Telerik.Blazor.Components;
+
+namespace CommonLib.Web.Source.Common.Components.ExtStepperComponent
+{
+    public class ExtStepperBase : MyComponentBase
+    {
+        [Parameter]
+        public TelerikStepper Ts { get; set; }
+
+		[Parameter]
+		public RenderFragment StepperSteps { get; set; }
+    }
+}
